174fd74bb0555b69986eaa77ec8fd7e08fe92a28
BuildTools <unconfigured@null.spigotmc.org>
2021-02-24 23:11:15 +0100

    added image model

---
+++ app/Models/Image.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $table = 'images';
+
+	protected $fillable = [
+        'name',
+        'path'
+    ];
+
+
+    public function bid() {
+        return $this->belongsTo('App\Item');
+    }
+}
