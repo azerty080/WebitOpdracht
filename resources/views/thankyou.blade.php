1694701365d1217d560d43570f69978f9bb65daa
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 22:30:25 +0100

    added thank you page

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title', 'Bedankt')
+
+
+@section('content')
+
+	<h1>Bedankt, je bod is geplaatst</h1>
+
+
+	<a href="{{ route('index') }}">Bekijk andere voorwerpen</a>
+
+
+@stop
